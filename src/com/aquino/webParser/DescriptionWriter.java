83484c9bdbff46ee7df7f847479e8b1b254c8134
alex <blackterrapin@gmail.com>
2018-04-14 14:32:50 +0900

    split functions in description writer. need to update usage in UI.

--- src/com/aquino/webParser/DescriptionWriter.java
+++ src/com/aquino/webParser/DescriptionWriter.java
@@ -24,29 +24,36 @@ import java.util.logging.Logger;
 public class DescriptionWriter {
     
     private static final Logger logger = Logger.getLogger(DescriptionWriter.class.getName());
+    private String bookDescriptions;
     
-    public boolean writeBooks(Book[] books, File file) {
+    public boolean writeBooks(Book[] books) {
+        StringBuilder sb = new StringBuilder(bookDescriptions);
+        for (Book book : books) { 
+                sb.append(String.format("책이름: %s%n책소개: %n%s%n%n%n",
+                        book.getTitle(),book.getDescription()));
+        }
+        bookDescriptions = sb.toString();
+        logger.log(Level.INFO, "Done getting descriptions");
+        return true;
+    }
+    
+    public boolean saveBooks(File file) {
         File saveFile = new File(file.getAbsoluteFile().getParent()
                 +"/"+ "Descriptions- "+LocalDateTime.now()
                         .format(DateTimeFormatter
                                 .ofPattern("yyyy MM dd k m"))
                         .toString()+".txt");
-        try (BufferedWriter bw = new BufferedWriter(
+         try (BufferedWriter bw = new BufferedWriter(
                         new OutputStreamWriter(
                                 new FileOutputStream(saveFile, true),"UTF-8"));) {
             bw.write(LocalDateTime.now().toString());
-            bw.newLine();
-            for (Book book : books) { 
-                
-                bw.write(String.format("책이름: %s%n책소개: %n%s%n%n%n",
-                        book.getTitle(),book.getDescription()));
-            }
+            bw.write(bookDescriptions);
         } catch (IOException  ex) {
             logger.log(Level.SEVERE, "Problem with writing the book {0}", ex.getMessage());
             return false;
         } 
         logger.log(Level.INFO, "Done writing book descriptions");
         return true;
-    }   
+    }
     
 }
