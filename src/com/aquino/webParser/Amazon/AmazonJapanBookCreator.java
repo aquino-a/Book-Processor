e5f532e7c025a86fe3e97ca2a6babd6a3130c2af
alex <blackterrapin@gmail.com>
2019-05-14 12:45:49 +0530

    added amazon creator, missing image,authors

--- src/com/aquino/webParser/Amazon/AmazonJapanBookCreator.java
+++ src/com/aquino/webParser/Amazon/AmazonJapanBookCreator.java
@@ -3,30 +3,159 @@ package com.aquino.webParser.Amazon;
 import com.aquino.webParser.Book;
 import com.aquino.webParser.Utilities.Connect;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AmazonJapanBookCreator {
 
 
-    //https://www.amazon.co.jp/s?k=%s
+    private static final String searchUrlFormat = "https://www.amazon.co.jp/s?k=%s";
     private static final String kinoBookUrlFormat = "https://www.kinokuniya.co.jp/f/dsg-01-%s";
+    private static final Logger logger = Logger.getLogger(AmazonJapanBookCreator.class.getName());
+    private static final DateTimeFormatter sourceFormatter = DateTimeFormatter.ofPattern("yyyy/M/d");
+    private static final DateTimeFormatter targetFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
-    public Book createBookFromBookPage(String bookPage) throws IOException {
+    public Book createBook(String isbn){
+        Document doc = Connect.connectToURL(String.format(searchUrlFormat, isbn));
+        return null;
+    }
+
+
+    public Book createBookFromBookPage(String bookPageUrl) throws IOException {
         Book book = new Book();
-        Document doc = Connect.connectToURL(bookPage);
+        Document doc = Connect.connectToURL(bookPageUrl);
         if(doc == null)
-            throw new IOException(String.format("Document wasn't loaded: %s",bookPage));
+            throw new IOException(String.format("Document wasn't loaded: %s",bookPageUrl));
         return fillInBasicData(book, doc);
 
     }
 
     private Book fillInBasicData(Book book, Document doc) {
-
+        book.setOriginalPriceNumber(parsePrice(doc));
+        book.setTitle(parseTitle(doc));
+        book = parseAuthorDetails(book, doc);
+        book = parseSecondDetailSection(book, doc);
         return book;
     }
 
+    private Book parseAuthorDetails(Book book, Document doc) {
+        try{
+            Elements es = doc.getElementById("bylineInfo").getElementsByClass("author notFaded");
+            int authorCount = 0;
+            String contributionType = null;
+            for (Element e : es) {
+                contributionType = e.getElementsByClass("contribution").first().ownText();
+                if(contributionType.contains("翻訳"))
+                    book.setTranslator(findContributorName(e));
+                else if(contributionType.contains("著")){
+                    if(authorCount == 0){
+                        authorCount++;
+                        book.setAuthor(findContributorName(e));
+                    } else if (authorCount == 1){
+                        authorCount++;
+                        book.setAuthor(findContributorName(e));
+                    } else if(authorCount > 1){
+                        book.setAuthor("1494");
+                        book.setAuthor2("");
+                    }
+                }
+            }
 
+        }catch (Exception e){
+            logger.log(Level.WARNING, String.format("Couldn't parse author details: %s", e.getMessage()));
+            e.printStackTrace();
+//            book.setAuthor("");
+//            book.setAuthor2("");
+//            book.setTranslator("");
+//            book.setEnglishTitle("");
+        }
+        finally {
+            //TODO implement this;
+//            setDefaultAutorSection(book);
+            return book;
+        }
+    }
+
+    private String findContributorName(Element e) {
+        return e.getElementsByTag("a").first().ownText();
+    }
+
+    private int parsePrice(Document doc) {
+        String priceSource = doc.getElementsByClass("a-size-base a-color-price a-color-price").first().text().trim();
+        try {
+            return NumberFormat.getInstance(Locale.JAPAN).parse(priceSource.replace("￥","").trim()).intValue();
+        } catch (ParseException e) {
+            return -1;
+        }
+    }
+
+    private String parseTitle(Document doc) {
+        String titleSource = doc.getElementById("productTitle").ownText().trim();
+        if(titleSource.indexOf("(") > -1)
+            return titleSource.substring(0, titleSource.indexOf("("));
+        else return titleSource;
+    }
+
+    private Book parseSecondDetailSection(Book book, Document doc) {
+        Elements es =  doc.getElementById("detail_bullets_id").getElementsByTag("ul").first().getElementsByTag("li");
+        for (Element e : es ){
+            String whole = e.wholeText();
+            if(whole.contains("ページ"))
+                book.setPages(findPages(e.ownText()));
+            else if(whole.contains("出版社"))
+                book.setPublisher(findPublisher(e.ownText().trim()));
+            else if(whole.contains("発売日"))
+                book.setPublishDateFormatted(findPublishedDateFormatted(e.ownText().trim()));
+            else if(whole.contains("梱包サイズ"))
+                book.setBookSizeFormatted(findBookSizeFormatted(e.ownText().trim()));
+        }
+        return book;
+    }
+    private int findPages(String pagesSource) {
+        try {
+            return Integer.parseInt(pagesSource.substring(0,pagesSource.indexOf("ページ")));
+        } catch (NumberFormatException e){
+            return -1;
+        }
+    }
+
+    private String findPublisher(String publisherSource) {
+        if(publisherSource.contains(";"))
+            return publisherSource.substring(0, publisherSource.indexOf(";")).trim();
+        else if(publisherSource.contains("("))
+            return publisherSource.substring(0,publisherSource.indexOf("(")).trim();
+        logger.log(Level.WARNING, String.format("Couldn't find publisher: %s", publisherSource));
+        return "";
+    }
+
+    private String findPublishedDateFormatted(String dateSource) {
+        //MM/DD/YYYY
+        try {
+            LocalDate date = LocalDate.parse(dateSource, sourceFormatter);
+            return date.format(targetFormatter);
+        } catch (DateTimeParseException e){
+            logger.log(Level.WARNING, String.format("Couldn't find publish date: %s", dateSource));
+            return "";
+        }
+    }
+
+    private String findBookSizeFormatted(String sizeSource) {
+        //28.5 x 20.9 x 1.4 cm
+        if(sizeSource.lastIndexOf("x") > -1)
+            return sizeSource.substring(0, sizeSource.lastIndexOf("x")).trim();
+        logger.log(Level.WARNING, String.format("Couldn't find book size: %s", sizeSource));
+        return "";
+    }
 
 
 }
---
+++ src/com/aquino/webParser/Book.java
@@ -0,0 +1,226 @@
+package com.aquino.webParser;
+
+public class Book {
+
+    private String title, publishDate, originalPrice, bookSize,
+            cover, publishDateFormatted, bookSizeFormatted,
+            imageURL, author, englishTitle, translator,
+            publisher, author2, isbnString, description, category, authorOriginal,
+            locationUrl, kIsbn;
+    private long isbn, oclc;
+    private boolean titleExists;
+
+    private int pages;
+    private int weight;
+
+    private int originalPriceNumber;
+    private double originalPriceFormatted;
+
+    public boolean isTitleExists() {
+        return titleExists;
+    }
+
+    public void setTitleExists(boolean titleExists) {
+        this.titleExists = titleExists;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(String originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public String getBookSize() {
+        return bookSize;
+    }
+
+    public void setBookSize(String bookSize) {
+        this.bookSize = bookSize;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getPublishDateFormatted() {
+        return publishDateFormatted;
+    }
+
+    public void setPublishDateFormatted(String publishDateFormatted) {
+        this.publishDateFormatted = publishDateFormatted;
+    }
+
+    public String getBookSizeFormatted() {
+        return bookSizeFormatted;
+    }
+
+    public void setBookSizeFormatted(String bookSizeFormatted) {
+        this.bookSizeFormatted = bookSizeFormatted;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEnglishTitle() {
+        return englishTitle;
+    }
+
+    public void setEnglishTitle(String englishTitle) {
+        this.englishTitle = englishTitle;
+    }
+
+    public String getTranslator() {
+        return translator;
+    }
+
+    public void setTranslator(String translator) {
+        this.translator = translator;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAuthor2() {
+        return author2;
+    }
+
+    public void setAuthor2(String author2) {
+        this.author2 = author2;
+    }
+
+    public String getIsbnString() {
+        return isbnString;
+    }
+
+    public void setIsbnString(String isbnString) {
+        this.isbnString = isbnString;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthorOriginal() {
+        return authorOriginal;
+    }
+
+    public void setAuthorOriginal(String authorOriginal) {
+        this.authorOriginal = authorOriginal;
+    }
+
+    public String getLocationUrl() {
+        return locationUrl;
+    }
+
+    public void setLocationUrl(String locationUrl) {
+        this.locationUrl = locationUrl;
+    }
+
+    public String getkIsbn() {
+        return kIsbn;
+    }
+
+    public void setkIsbn(String kIsbn) {
+        this.kIsbn = kIsbn;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public long getOclc() {
+        return oclc;
+    }
+
+    public void setOclc(long oclc) {
+        this.oclc = oclc;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getOriginalPriceFormatted() {
+        return originalPriceFormatted;
+    }
+
+    public void setOriginalPriceFormatted(double originalPriceFormatted) {
+        this.originalPriceFormatted = originalPriceFormatted;
+    }
+
+    public int getOriginalPriceNumber() {
+        return originalPriceNumber;
+    }
+
+    public void setOriginalPriceNumber(int parsePrice) {
+        originalPriceNumber = parsePrice;
+    }
+}
