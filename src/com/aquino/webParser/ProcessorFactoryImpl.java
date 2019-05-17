43dcea0f61653a5afb068d3cec42e55aca50d314
alex <blackterrapin@gmail.com>
2019-05-17 11:12:05 +0530

    added mapper,apikey to factory

--- src/com/aquino/webParser/ProcessorFactoryImpl.java
+++ src/com/aquino/webParser/ProcessorFactoryImpl.java
@@ -6,14 +6,19 @@ import com.aquino.webParser.bookCreators.amazon.AmazonJapanBookCreator;
 import com.aquino.webParser.bookCreators.BookCreator;
 import com.aquino.webParser.bookCreators.aladin.AladinBookCreator;
 import com.aquino.webParser.oclc.OclcService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Properties;
 
 public class ProcessorFactoryImpl {
 
     private BookWindowService bookWindowService;
     private HashMap<BookCreatorType, BookCreator> bookCreatorHashMap = new HashMap<>();
     private OclcService oclcService;
+    private ObjectMapper mapper;
+    private String aladinApiKey;
 
     public BookWindowService CreateWindowService(){
         if(bookWindowService == null)
@@ -21,12 +26,12 @@ public class ProcessorFactoryImpl {
         return bookWindowService;
     }
 
-    public BookCreator CreateBookCreator(BookCreatorType creatorType){
+    public BookCreator CreateBookCreator(BookCreatorType creatorType) throws IOException {
         if(!bookCreatorHashMap.containsKey(creatorType)) {
             BookCreator newCreator;
             switch(creatorType){
-                case AladinApi: newCreator = new AladinBookCreator(); break;
-                case AmazonJapan: newCreator = new AmazonJapanBookCreator(); break;
+                case AladinApi: newCreator = new AladinBookCreator(getAladinApiKey(), CreateWindowService(), CreateOclcService(),CreateObjectMapper()); break;
+                case AmazonJapan: newCreator = new AmazonJapanBookCreator(CreateWindowService(), CreateOclcService()); break;
                 default: throw new UnsupportedOperationException(creatorType.toString());
             }
             bookCreatorHashMap.put(creatorType, newCreator);
@@ -34,10 +39,29 @@ public class ProcessorFactoryImpl {
         return bookCreatorHashMap.get(creatorType);
     }
 
+    private ObjectMapper CreateObjectMapper() {
+        if(mapper == null)
+            mapper = new ObjectMapper();
+        return mapper;
+    }
+
     public OclcService CreateOclcService(){
         if(oclcService == null)
             oclcService = new OclcServiceImpl();
         return oclcService;
+    }
+
+    private String getAladinApiKey() throws IOException {
+        if(aladinApiKey == null)
+            loadProperties();
+        return aladinApiKey;
+    }
+
+    private void loadProperties() throws IOException {
+        Properties prop = new Properties();
+        prop.load(ProcessorFactoryImpl.class.getClassLoader()
+                .getResourceAsStream("com/aquino/webParser/resources/config.properties"));
+        aladinApiKey = prop.getProperty("aladin.api.key");
     }
 
 
