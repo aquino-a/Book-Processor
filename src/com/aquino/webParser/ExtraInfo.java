4a5d49acf5270e835665dc407fb6b3a6079632d3
Alex <aquinobean@gmail.com>
2020-09-28 13:09:08 +0900

    added extraInfo class

---
+++ src/com/aquino/webParser/ExtraInfo.java
@@ -0,0 +1,44 @@
+package com.aquino.webParser;
+
+public class ExtraInfo {
+
+    private int columnNumber;
+    private String value;
+    private Type type;
+
+    public ExtraInfo(int columnNumber, String value, Type type) {
+        this.columnNumber = columnNumber;
+        this.value = value;
+        this.type = type;
+    }
+
+    public int getColumnNumber() {
+        return columnNumber;
+    }
+
+    public void setColumnNumber(int columnNumber) {
+        this.columnNumber = columnNumber;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public enum Type {
+        HyperLink
+    }
+}
+
+
