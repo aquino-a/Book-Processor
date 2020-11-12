71c91b721c4bcf6f2385a16e3549410bb452e690
Alex <aquinobean@gmail.com>
2020-11-12 13:12:08 +0900

    finish BookWindowServiceImpl.addAuthor was missing in last commit

--- src/com/aquino/webParser/BookWindowServiceImpl.java
+++ src/com/aquino/webParser/BookWindowServiceImpl.java
@@ -10,6 +10,7 @@ import org.jsoup.nodes.Element;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
 
@@ -21,27 +22,98 @@ public class BookWindowServiceImpl implements BookWindowService {
     private static final String SET_NATIVE_URL = "https://www.bookswindow.com/admin/shared/ajax/language_obj_set_native_process";
     private static final String AUTHOR_ROLE = "100";
     private static final String PUBLISHER_ROLE = "100";
+    private static final String ENGLISH_LANG_CODE = "1000";
+    private static final String AUTHOR_EDIT_URL_FORMAT = "https://www.bookswindow.com/admin/author/%s/edit/main";
     public static final Pattern AUTHOR_NUMBER_REGEX = Pattern.compile("\"redirect_url\":\"\\\\/admin\\\\/author\\\\/(\\d+)\\\\/edit\\\\/main");
     //add author return url
     //https://www.bookswindow.com/admin/author/(33521)/edit/main
 
 //Ignore all empty strings
-    private static final Map<String, String> ADD_LANGUAGE_PARAMS = Map.of(
-            "input_arr[prj_language]", "langCode",
-            "input_arr[first_name]", "firstName",
+//    private static final Map<String, String> ADD_LANGUAGE_PARAMS = Map.of(
+//            "input_arr[prj_language]", "langCode",
+//            "input_arr[first_name]", "firstName",
+//            "input_arr[first_name_romanized]", "",
+//            "input_arr[last_name]", "lastName",
+//            "input_arr[last_name_romanized]", "",
+//            "input_arr[description]", "",
+//            "opt_param[table]", "author", //don't change
+//            "opt_param[pk]", "authorId",
+//            "opt_param[field]", "language_obj", //don't change
+//            "opt_param[has_native]", "1", //don't change
+//            "opt_param[info_arr][0][key]", "first_name",
+//            "opt_param[info_arr][0][label]", "First Name",
+//            "opt_param[info_arr][0][required]", "true",
+//            "opt_param[info_arr][0][class]", "input-xlarge",
+//            "opt_param[info_arr][1][key]", "first_name_romanized",
+//            "opt_param[info_arr][1][label]", "Romanized",
+//            "opt_param[info_arr][1][class]", "input-xlarge",
+//            "opt_param[info_arr][1][input_type]", "input_indented",
+//            "opt_param[info_arr][2][key]", "last_name",
+//            "opt_param[info_arr][2][label]", "Last Name",
+//            "opt_param[info_arr][2][class]", "input-xlarge",
+//            "opt_param[info_arr][3][key]", "last_name_romanized",
+//            "opt_param[info_arr][3][label]", "Romanized",
+//            "opt_param[info_arr][3][class]", "input-xlarge",
+//            "opt_param[info_arr][3][input_type]", "input_indented",
+//            "opt_param[info_arr][4][key]", "description",
+//            "opt_param[info_arr][4][label]", "Intro / Description",
+//            "opt_param[info_arr][4][class]", "input-xxlarge",
+//            "opt_param[info_arr][4][input_type]", "textarea",
+//            "opt_param[sort_arr][]", "first_name",
+//            "opt_param[sort_arr][]", "asc"
+//            );
+
+    private static final Map<String, String> ADD_LANGUAGE_PARAMS1 = Map.of(
             "input_arr[first_name_romanized]", "",
-            "input_arr[last_name]", "lastName",
             "input_arr[last_name_romanized]", "",
             "input_arr[description]", "",
             "opt_param[table]", "author", //don't change
-            "opt_param[pk]", "authorId",
-            "opt_param[field]", "authorId",
-            "opt_param[has_native]", "1"
-            );
+            "opt_param[field]", "language_obj", //don't change
+            "opt_param[has_native]", "1", //don't change
+            "opt_param[info_arr][0][key]", "first_name"
+    );
+
+
+    private static final Map<String, String> ADD_LANGUAGE_PARAMS2 = Map.of(
+            "opt_param[info_arr][0][label]", "First Name",
+            "opt_param[info_arr][0][required]", "true",
+            "opt_param[info_arr][0][class]", "input-xlarge",
+            "opt_param[info_arr][1][key]", "first_name_romanized",
+            "opt_param[info_arr][1][label]", "Romanized",
+            "opt_param[info_arr][1][class]", "input-xlarge",
+            "opt_param[info_arr][1][input_type]", "input_indented",
+            "opt_param[info_arr][2][key]", "last_name",
+            "opt_param[info_arr][2][label]", "Last Name",
+            "opt_param[info_arr][2][class]", "input-xlarge"
+    );
+
+
+    private static final Map<String, String> ADD_LANGUAGE_PARAMS3 = Map.of(
+            "opt_param[info_arr][3][key]", "last_name_romanized",
+            "opt_param[info_arr][3][label]", "Romanized",
+            "opt_param[info_arr][3][class]", "input-xlarge",
+            "opt_param[info_arr][3][input_type]", "input_indented",
+            "opt_param[info_arr][4][key]", "description",
+            "opt_param[info_arr][4][label]", "Intro / Description",
+            "opt_param[info_arr][4][class]", "input-xxlarge",
+            "opt_param[info_arr][4][input_type]", "textarea",
+            "opt_param[sort_arr][]", "first_name"
+    );
+
+    private static Map<String, String> GetAddLanguageParams(){
+        var m = new HashMap<String, String>(ADD_LANGUAGE_PARAMS1);
+        m.putAll(ADD_LANGUAGE_PARAMS2);
+        m.putAll(ADD_LANGUAGE_PARAMS3);
+        return m;
+    }
+
+    public static String GetEditAuthorUrl(int id){
+        return String.format(AUTHOR_EDIT_URL_FORMAT, String.valueOf(id));
+    }
 
     private static final Map<String, String> SET_NATIVE_PARAMS = Map.of(
             "native_lang", "langCode",
-            "opt_param[table]", "author",
+            "opt_param[table]", "author", //don't change
             "opt_param[pk]", "authorId",
             " opt_param[field]", "language_obj",
             "opt_param[has_native]", "1"
@@ -243,13 +315,6 @@ public class BookWindowServiceImpl implements BookWindowService {
         throw new NotImplementedException("TODO");
     }
 
-//    <html>
-// <head></head>
-//    <body>
-//    {"status":"success","message":"Successfully Added Author # 33525","redirect_url":"\/admin\/author\/33525\/edit\/main"}
-// </body>
-//</html>
-
     @Override
     public int addAuthor(Author author) {
 
@@ -264,15 +329,36 @@ public class BookWindowServiceImpl implements BookWindowService {
         result = Integer.parseInt(matcher.group(1).trim());
         AddEnglishLanguage(result, author);
         AddNativeLanguage(result, author);
+        SetNativeLanguage(result, author);
         return result;
     }
 
-    private void AddEnglishLanguage(int result, Author author) {
-//        Login.postDocument(ADD_LANGUAGE_URL,
-//                Map.of("first_name",author.getEnglishFirstName(),"middle_name", "","last_name", author.getEnglishLastName(), "contributor_role", AUTHOR_ROLE));
+    private void AddEnglishLanguage(int id, Author author) {
+        var data = GetAddLanguageParams();
+        data.put("input_arr[prj_language]", ENGLISH_LANG_CODE);
+        data.put("input_arr[first_name]", author.getEnglishFirstName());
+        data.put("input_arr[last_name]", author.getEnglishLastName());
+        data.put("opt_param[pk]", String.valueOf(id));
+
+        var doc = Login.postDocument(ADD_LANGUAGE_URL, data);
     }
 
-    private void AddNativeLanguage(int result, Author author) {
+    private void AddNativeLanguage(int id, Author author) {
+        var data = GetAddLanguageParams();
+        data.put("input_arr[prj_language]", author.getLanguage().LanguageCode);
+        data.put("input_arr[first_name]", author.getNativeFirstName());
+        data.put("input_arr[last_name]", author.getNativeLastName());
+        data.put("opt_param[pk]", String.valueOf(id));
+
+        var doc = Login.postDocument(ADD_LANGUAGE_URL, data);
+    }
+
+    private void SetNativeLanguage(int id, Author author) {
+        var data = new HashMap<String, String>(SET_NATIVE_PARAMS);
+        data.replace("native_lang", author.getLanguage().LanguageCode);
+        data.replace("opt_param[pk]", String.valueOf(id));
+
+        var doc = Login.postDocument(SET_NATIVE_URL, data);
     }
 
     @Override
