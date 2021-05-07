418eb30541032dde9332c9ed4d7bfba38a6b581e
Alex <29205589+aquino-a@users.noreply.github.com>
2021-05-07 16:22:53 +0900

    add JapaneseAuthorStrategyIntegratedTest

---
+++ src/test/java/com/aquino/webParser/autofill/JapaneseAuthorStrategyIntegratedTest.java
@@ -0,0 +1,18 @@
+package com.aquino.webParser.autofill;
+
+import com.aquino.webParser.model.Book;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class JapaneseAuthorStrategyIntegratedTest {
+
+    @Test
+    public void createAuthor() {
+        var strategy = new JapaneseAuthorStrategy();
+        var book = new Book();
+        book.setAuthor("ハンス・ロスリング");
+        var result = strategy.createAuthor(book);
+        Assert.assertEquals("hansu rosurin gu", result.getEnglishLastName());
+    }
+}
