38494a0abaf411225945d6a37009b7e0c5a585fc
Alex <29205589+aquino-a@users.noreply.github.com>
2021-05-13 13:22:35 +0900

    add function to set the current directory, add xlsm to file filter

--- src/main/java/com/aquino/webParser/utilities/FileUtility.java
+++ src/main/java/com/aquino/webParser/utilities/FileUtility.java
@@ -5,17 +5,59 @@
  */
 package com.aquino.webParser.utilities;
 
-import java.io.File;
-import javax.swing.JComponent;
-import javax.swing.JFileChooser;
+import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+import java.util.Properties;
 
 /**
- *
  * @author alex
  */
 public class FileUtility {
-    
+
+    private static final String DIRECTORY_PROPERTY = "directory";
+    private static final JFileChooser FILE_CHOOSER = createFileChooser();
+
+    private static JFileChooser createFileChooser() {
+        var path = getCurrentDirectoryPath();
+
+        var fc = new JFileChooser();
+        if (path.isPresent()) {
+            fc.setCurrentDirectory(new File(path.get()));
+        }
+
+        var filter = new FileNameExtensionFilter("Excel", "xlsx", "xlsm");
+        fc.setFileFilter(filter);
+        return fc;
+    }
+
+    private static Optional<String> getCurrentDirectoryPath() {
+
+        try {
+            var prop = new Properties();
+            prop.load(FileUtility.class.getClassLoader()
+                .getResourceAsStream("config.properties"));
+            if (!prop.containsKey(DIRECTORY_PROPERTY)) {
+                return Optional.empty();
+            }
+
+            var directory = prop.getProperty(DIRECTORY_PROPERTY);
+            var path = Path.of(directory);
+            if (!Files.exists(path)) {
+                return Optional.empty();
+            }
+
+            return Optional.of(path.toAbsolutePath().toString());
+        }
+        catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
     //Returns null if nothing is selected.  Check for null.
     public static File openFile(JComponent component) {
         JFileChooser fc = getFileChooser();
@@ -23,18 +65,18 @@ public class FileUtility {
             return fc.getSelectedFile();
         return null;
     }
+
     //Returns null if nothing is selected. Check for null
     public static File saveLocation(JComponent component) {
         JFileChooser fc = getFileChooser();
-        if(fc.showSaveDialog(component) == JFileChooser.APPROVE_OPTION) {
+        if (fc.showSaveDialog(component) == JFileChooser.APPROVE_OPTION) {
             return fc.getSelectedFile();
         }
-            
+
         return null;
     }
+
     private static JFileChooser getFileChooser() {
-        JFileChooser fc = new JFileChooser();
-        fc.setFileFilter(new FileNameExtensionFilter("Excel", "xlsx"));
-        return fc;
+        return FILE_CHOOSER;
     }
 }
