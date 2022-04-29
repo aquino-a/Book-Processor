397ae5a37bc9ce3a094386bf879f3bb269b322fb
Alex <29205589+aquino-a@users.noreply.github.com>
2022-04-29 20:49:26 +0900

    add fx main

---
+++ fx/src/main/java/com/aquino/webParser/fx/Main.java
@@ -0,0 +1,24 @@
+package com.aquino.webParser.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        var fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
+        var scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Jeein's Web Parser");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
