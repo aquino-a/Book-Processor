84a8a368b25cb315e9e65026821f9322b739a4ac
alex <alexaquino@daum.net>
2023-07-30 07:26:47 +0900

    add honya test and fix

--- core/src/test/java/com/aquino/webParser/bookCreators/honya/HonyaClubBookCreatorIntegrationTest.java
+++ core/src/test/java/com/aquino/webParser/bookCreators/honya/HonyaClubBookCreatorIntegrationTest.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.notNull;
+
 public class HonyaClubBookCreatorIntegrationTest {
 
     private HonyaClubBookCreator bookCreator;
@@ -21,5 +25,14 @@ public class HonyaClubBookCreatorIntegrationTest {
         Book book = bookCreator.createBookFromIsbn("9784838731275");
         Assert.assertEquals("https://www.honyaclub.com/shop/g/g20026713/", book.getBookPageUrl());
         Assert.assertTrue(book.getDescription().contains("これまでのアイドル写真集とは一線を画す"));
+    }
+
+    @Test
+    public void createBookFromIsbn2() throws IOException {
+        var book = bookCreator.createBookFromIsbn("9784022518965");
+
+        assertThat(book.getCategory(), is("人文 > 社会・文化 > 地方文化"));
+        assertThat(book.getOriginalPriceNumber(), is(2_420));
+        assertThat(book.getDescription(), is(not(emptyOrNullString())));
     }
 }
