00d37e07224c3333b8fb7c735e4ee19dcf765910
alex <alexaquino@daum.net>
2023-07-30 10:11:38 +0900

    update kino test

--- core/src/test/java/com/aquino/webParser/bookCreators/kino/KinoBookCreatorTest.java
+++ core/src/test/java/com/aquino/webParser/bookCreators/kino/KinoBookCreatorTest.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
+
+import com.aquino.webParser.BookWindowService;
+import com.aquino.webParser.bookCreators.honto.HontoBookCreator;
+import com.aquino.webParser.bookCreators.honya.HonyaClubBookCreator;
+import com.aquino.webParser.chatgpt.ChatGptService;
+import com.aquino.webParser.oclc.OclcService;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -12,11 +19,21 @@ import static org.hamcrest.Matchers.*;
 public class KinoBookCreatorTest {
 
     private KinoBookCreator bc;
+    @Mock
+    BookWindowService bookWindowService;
+    @Mock
+    OclcService oclcService;
+    @Mock
+    ChatGptService chatGptService;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        bc = new KinoBookCreator();
+        bc = new KinoBookCreator(
+            bookWindowService, 
+            chatGptService,
+            new HontoBookCreator(),
+            new HonyaClubBookCreator());
     }
 
     @Test
