430e14d8ee59b304df112154542f9ed9181fc3b0
alex <alexaquino@daum.net>
2023-07-30 10:07:08 +0900

    remove amazon, add kino to factory

--- core/src/main/java/com/aquino/webParser/ProcessorFactoryImpl.java
+++ core/src/main/java/com/aquino/webParser/ProcessorFactoryImpl.java
@@ -37,14 +37,15 @@ import java.util.stream.Stream;
 public class ProcessorFactoryImpl {
 
     /**
-     * A map containing the locations of the book properties needed for reading an excel.
+     * A map containing the locations of the book properties needed for reading an
+     * excel.
      */
-    private static final Map<String, Integer> BOOK_PROPERTY_EXCEL_MAP = Stream.of(new Object[][]{
-        {"isbn", 0},
-        {"oclc", 3},
-        {"author", 10},
-        {"author2", 13},
-        {"publisher", 16}
+    private static final Map<String, Integer> BOOK_PROPERTY_EXCEL_MAP = Stream.of(new Object[][] {
+            { "isbn", 0 },
+            { "oclc", 3 },
+            { "author", 10 },
+            { "author2", 13 },
+            { "publisher", 16 }
     }).collect(Collectors.toMap(data -> (String) data[0], data -> (int) data[1]));
     private static final ObjectMapper OBJECT_MAPPER = createMapper();
 
@@ -60,48 +61,49 @@ public class ProcessorFactoryImpl {
     private String openaiApiKey;
     private List<Category> categories;
 
-
-    public BookWindowService CreateWindowService() {
+    public BookWindowService createWindowService() {
         if (bookWindowService == null)
             bookWindowService = new BookWindowServiceImpl();
         return bookWindowService;
     }
 
-    public BookCreator CreateBookCreator(BookCreatorType creatorType) throws IOException {
+    public BookCreator createBookCreator(BookCreatorType creatorType) throws IOException {
         if (!bookCreatorHashMap.containsKey(creatorType)) {
             BookCreator newCreator;
             switch (creatorType) {
                 case AladinApi:
-                    newCreator = new AladinBookCreator(CreateWindowService(), CreateOclcService(), createChatGptService());
+                    newCreator = new AladinBookCreator(createWindowService(), createOclcService(),
+                            createChatGptService());
                     break;
-                case AmazonJapan: {
-                    AmazonJapanBookCreator abc = new AmazonJapanBookCreator(CreateWindowService(), CreateOclcService(), createChatGptService());
-                    abc.setHontoBookCreator(new HontoBookCreator());
-                    abc.setHonyaClubBookCreator(new HonyaClubBookCreator());
-                    abc.setYahooBookCreator(new YahooBookCreator());
-                    abc.setWorldCatBookCreator(new WorldCatBookCreator());
-                    abc.setKinoBookCreator(new KinoBookCreator());
-                    newCreator = abc;
+                case KinoHontoHonya:
+                    var japaneseCreator = new KinoBookCreator(
+                            createWindowService(),
+                            createChatGptService(),
+                            new HontoBookCreator(),
+                            new HonyaClubBookCreator());
+                    japaneseCreator.setYahoo(new YahooBookCreator());
 
-                }
-                break;
+                    newCreator = japaneseCreator;
+                    break;
                 default:
                     throw new UnsupportedOperationException(creatorType.toString());
             }
+
             bookCreatorHashMap.put(creatorType, newCreator);
         }
+
         return bookCreatorHashMap.get(creatorType);
     }
 
-    public OclcService CreateOclcService() {
+    public OclcService createOclcService() {
         if (oclcService == null)
             oclcService = new OclcServiceImpl();
         return oclcService;
     }
-    
-    public ChatGptService createChatGptService() throws IOException{
+
+    public ChatGptService createChatGptService() throws IOException {
         var chatGptService = new ChatGptServiceImpl(
-                OBJECT_MAPPER, 
+                OBJECT_MAPPER,
                 getOpenAiApiKey(),
                 createSummaryRepository());
         chatGptService.setCategories(categories);
@@ -118,7 +120,7 @@ public class ProcessorFactoryImpl {
             loadProperties();
         return aladinApiKey;
     }
-    
+
     public String getOpenAiApiKey() throws IOException {
         if (openaiApiKey == null)
             loadProperties();
@@ -134,12 +136,13 @@ public class ProcessorFactoryImpl {
     private void loadProperties() throws IOException {
         Properties prop = new Properties();
         prop.load(ProcessorFactoryImpl.class.getClassLoader()
-            .getResourceAsStream("config.properties"));
+                .getResourceAsStream("config.properties"));
         aladinApiKey = prop.getProperty("aladin.api.key");
         openaiApiKey = prop.getProperty("openai.api.key");
-        try(var stream = ProcessorFactoryImpl.class.getClassLoader()
+        try (var stream = ProcessorFactoryImpl.class.getClassLoader()
                 .getResourceAsStream("categories.json")) {
-            categories = OBJECT_MAPPER.readValue(stream, new TypeReference<List<Category>>(){});
+            categories = OBJECT_MAPPER.readValue(stream, new TypeReference<List<Category>>() {
+            });
         }
     }
 
@@ -149,30 +152,28 @@ public class ProcessorFactoryImpl {
 
     public AutoFillService GetAutoFillService() throws IOException, URISyntaxException {
         var autoFillService = new AutoFillServiceImpl(
-            (OclcServiceImpl) this.CreateOclcService(),
-            this.CreateWindowService(),
-            this.GetExcelMap(), getAuthorStrategies());
+                (OclcServiceImpl) this.createOclcService(),
+                this.createWindowService(),
+                this.GetExcelMap(), getAuthorStrategies());
         return autoFillService;
     }
 
     private Map<Language, AuthorStrategy> getAuthorStrategies() throws IOException, URISyntaxException {
         return Map.of(
-            Language.Korean, new KoreanAuthorStrategy(GetKoreanLastNames()),
-            Language.Japanese, new JapaneseAuthorStrategy()
-        );
+                Language.Korean, new KoreanAuthorStrategy(GetKoreanLastNames()),
+                Language.Japanese, new JapaneseAuthorStrategy());
     }
 
     public Map<String, String> GetKoreanLastNames() throws URISyntaxException, IOException {
         if (koreanLastNames == null) {
             try (
-                var lastNamesStream = this.getClass().getClassLoader()
-                    .getResourceAsStream("korean-last-names.csv");
-                var br = new BufferedReader(new InputStreamReader(lastNamesStream, StandardCharsets.UTF_8))
-            ) {
+                    var lastNamesStream = this.getClass().getClassLoader()
+                            .getResourceAsStream("korean-last-names.csv");
+                    var br = new BufferedReader(new InputStreamReader(lastNamesStream, StandardCharsets.UTF_8))) {
                 koreanLastNames = br.lines()
-                    .map(name -> name.split(","))
-                    .filter(array -> array.length == 2)
-                    .collect(Collectors.toMap(array -> array[0].strip(), array -> array[1].strip()));
+                        .map(name -> name.split(","))
+                        .filter(array -> array.length == 2)
+                        .collect(Collectors.toMap(array -> array[0].strip(), array -> array[1].strip()));
             }
         }
         return koreanLastNames;
