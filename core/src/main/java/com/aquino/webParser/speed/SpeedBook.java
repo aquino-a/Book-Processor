3ac89b2503cb989102bb1d17ba78685646dfb6c3
Alex <29205589+aquino-a@users.noreply.github.com>
2024-02-28 20:42:22 -0600

    start downloader

--- core/src/main/java/com/aquino/webParser/speed/SpeedBook.java
+++ core/src/main/java/com/aquino/webParser/speed/SpeedBook.java
@@ -1,5 +1,7 @@
 package com.aquino.webParser.speed;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -8,4 +10,40 @@ public class SpeedBook {
     
     @Id
     private long speedBookId;
+
+    private LocalDate incompletePublishDate;
+    private LocalDate publishDate;
+    private String itemUrl;
+
+    public long speedBookId() {
+        return speedBookId;
+    }
+
+    public void setSpeedBookId(long speedBookId) {
+        this.speedBookId = speedBookId;
+    }
+
+    public LocalDate incompletePublishDate() {
+        return incompletePublishDate;
+    }
+
+    public void setIncompletePublishDate(LocalDate incompletePublishDate) {
+        this.incompletePublishDate = incompletePublishDate;
+    }
+
+    public LocalDate publishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String itemUrl() {
+        return itemUrl;
+    }
+    
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
 }
