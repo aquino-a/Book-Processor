ee611726868be56ae200d022005fec4557949305
aquino-a <29205589+aquino-a@users.noreply.github.com>
2025-12-09 21:10:11 -0600

    use grok 2 in test

--- core/src/test/java/com/aquino/webParser/chatgpt/ChatGptServiceImplTest.java
+++ core/src/test/java/com/aquino/webParser/chatgpt/ChatGptServiceImplTest.java
@@ -44,7 +44,7 @@ public class ChatGptServiceImplTest {
         chat.setCategories(factory.getCategories());
 
         var grokKey = factory.getGrokApiKey();
-        var grok = new GrokService(objectMapper, grokKey, summaryRepository, "grok-3-mini");
+        var grok = new GrokService(objectMapper, grokKey, summaryRepository, "grok-2-latest");
         grok.setCategories(factory.getCategories());
 
         return Arrays.asList(new Object[][] {
